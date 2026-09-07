55e66edee6cc099565698942272dc6a18c9ce59d
zzpice <140223729+zzpice@users.noreply.github.com>
2026-09-06 21:23:51 -0700

    Refactor page code without behavior changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="bond-rebalancer-";
-const CACHE_NAME=`${CACHE_PREFIX}v4.8.1`;
+const CACHE_NAME=`${CACHE_PREFIX}v4.8.2`;
 const appURL=path=>new URL(path,self.location.href).href;
 const INDEX_URL=appURL("./index.html");
 const APP_SHELL=[
